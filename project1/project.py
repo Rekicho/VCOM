f956d3d1681aaada105eafc0b89185f0d8ae8cca
Rekicho <up201604145@fe.up.pt>
2020-03-10 16:05:51 +0000

    Basic red circle detection

--- project1/project.py
+++ project1/project.py
@@ -1,9 +1,11 @@
-import cv2, sys
+import cv2
+import sys
+import numpy as np
 
 FPS = 60
 
 def openImage(name):
-    return cv2.imread(name)
+    return cv2.imread(name,cv2.IMREAD_COLOR)
 
 def takeImageFromCamera():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -21,6 +23,17 @@ if len(sys.argv) > 1:
     img = openImage(sys.argv[1])
 else:
     img = takeImageFromCamera()
+r = img.copy()
+r[:, :, 0] = 0
+r[:, :, 1] = 0
+gray = cv2.cvtColor(r, cv2.COLOR_BGR2GRAY)
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 cv2.imshow("Sign",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
