97f54914e69560ac02221b00cc4038e6707dabb3
Rekicho <up201604145@fe.up.pt>
2020-03-16 00:32:16 +0000

    Text recongicion on stop.png

--- project1/project.py
+++ project1/project.py
@@ -1,6 +1,8 @@
 import cv2
 import sys
 import numpy as np
+import pytesseract
+from pytesseract import Output
 
 FPS = 60
 
@@ -66,12 +68,28 @@ def detectStop(img):
 
     return img
 
+def detectStopText(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    low=np.array([150])
+    high=np.array([255])
+    mask = cv2.inRange(gray, low, high)
+    gray[mask > 0] = 0
+    # print(pytesseract.image_to_string(gray, lang='eng', config='--psm 10 --oem 3'))
+    d = pytesseract.image_to_data(gray, lang='eng', config='--psm 10 --oem 3', output_type=Output.DICT)
+    n_boxes = len(d['level'])
+    for i in range(n_boxes):
+        if d['text'][i] == 'STOP':
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return img
+
 if len(sys.argv) > 1: 
     img = openImage(sys.argv[1])
     #img = detectTriangles(img)
     #img = detectRectangles(img)
     #img = detectCircles(img)
-    img = detectStop(img)
+    # img = detectStop(img)
+    img = detectStopText(img)
 else:
     img = takeImageFromCamera()
 cv2.imshow("Sign",img)
