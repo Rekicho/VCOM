d4574d63a042bbcd1974c5d2184502e8bbdf29f2
Rekicho <up201604145@fe.up.pt>
2020-04-06 19:06:10 +0100

    Improve test.sh

--- project1/test.sh
+++ project1/test.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
+#Root JPG
 for file in res/*.jpg
 do
     if [ "$file" != "res/*.jpg" ] 
+    then
+        ( python3 src/project.py $file & );
+    fi 
+done
+
+#Root PNG
+for file in res/*.png
+do
+    if [ "$file" != "res/*.png" ] 
+    then
+        ( python3 src/project.py $file & );
+    fi 
+done
+
+#Signs JPG
+for file in res/signs/*.jpg
+do
+    if [ "$file" != "res/signs/*.jpg" ] 
+    then
+        ( python3 src/project.py $file & );
+    fi 
+done
+
+#Signs PNG
+for file in res/signs/*.png
+do
+    if [ "$file" != "res/signs/*.png" ] 
     then
         ( python3 src/project.py $file & );
     fi 
